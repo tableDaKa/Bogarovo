21b9505258057f26f0a03fde11a92b9bcacdd7c2
tableDaKa <karelbogar03@gmail.com>
2026-02-08 01:47:40 +0100

    Convert app to XML SMS sender

--- app/src/main/java/com/example/bogarovo/ui/theme/Color.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bogarovo.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val GreenPrimary = Color(0xFF2E7D32)
-val GreenSecondary = Color(0xFF81C784)
-val RedLowStock = Color(0xFFB71C1C)
